2cbcc0a80e7d51ff239286f2a92ef1c2819ce032
Catarina <catisousalima1999@gmail.com>
2023-03-31 10:32:48 +0000

    Initial commit

---
+++ src/main/java/hbv202g/zoomanagement/HelloController.java
@@ -0,0 +1,14 @@
+package hbv202g.zoomanagement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
